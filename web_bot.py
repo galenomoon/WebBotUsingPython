b2c04bace0640dcc35eeb5f749c5fbde866d22fc
Guilherme Galeno <94015295+galenomoon@users.noreply.github.com>
2023-05-13 13:15:47 -0300

    Update web_bot.py

--- web_bot.py
+++ web_bot.py
@@ -2,9 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 
-# contato@dentistaespecializado.com
-# zetalana1970
-
 
 def alert(type):
     if type == 'error':
